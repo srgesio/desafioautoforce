f9ce22c2fb634052f469636299914b230e95a89b
20171014040038 <gesio.m@academico.ifrn.edu.br>
2021-04-03 11:28:04 -0300

    parte2 incompleta

--- Parte1/parte1.js
+++ Parte1/parte1.js
@@ -1,2 +1,2 @@
 let networkList = document.getElementsByClassName('header__networks-list')[0]
-networkList.innerHTML = '<a href="https://www.youtube.com/channel/UCLI4tg1oh_oLiJJteExJdUQ" target="_blank"><i class="icon icon-youtube-header icon--small icon--hover-youtube"></i></a>'
+networkList.innerHTML += '<a href="https://www.youtube.com/channel/UCLI4tg1oh_oLiJJteExJdUQ" target="_blank"><i class="icon icon-youtube-header icon--small icon--hover-youtube"></i></a>'
--- Parte2/parte2.js
+++ Parte2/parte2.js
@@ -1,13 +1,19 @@
-let numbers = document.getElementsByClassName('header-mobile__whatsapp-link')
-let modal = document.getElementsByClassName('modal micromodal-slide')[0]
+// modal.classList.remove('is-open')
+// modal.setAttribute('aria-hidden', false)
+let modalGlobal = document.getElementsByClassName('header-conversion-form-whatsapp-modal')[0]
 
-modal.classList.remove('is-open')
-modal.setAttribute('aria-hidden', false)
+let modal = document.getElementsByClassName('modal micromodal-slide')[0]
+let numbers = document.getElementsByClassName('header-mobile__whatsapp-link')
+
+
 for (let i = 0; i < numbers.length; i++) {
     numbers[i].setAttribute('href', numbers[i].getAttribute('data-link'))
     numbers[i].addEventListener('click', (e) => {
         e.preventDefault()
         console.log('aaaaaaaaa')
+        if (modal.classList == 'modal micromodal-slide is-open') {
+            modal.classList = 'modal micromodal-slide'
+        }
 
     })
 }
