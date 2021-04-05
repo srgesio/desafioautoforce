661b1c7d7af4f17e33e4208eb2f8dca3b169f2b4
20171014040038 <gesio.m@academico.ifrn.edu.br>
2021-04-05 02:28:12 -0300

    desafio finalizado

--- Parte2/parte2.js
+++ Parte2/parte2.js
@@ -5,8 +5,6 @@ let numbers = document.getElementsByClassName('header-mobile__whatsapp-link')
 for (let i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener('click', (e) => {
         e.preventDefault()
-        console.log(numbers[i].getAttribute('data-link'))
-        numbers[i].setAttribute('href', numbers[i].getAttribute('data-link'))
         window.location.href = numbers[i].getAttribute('data-link')
     })
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # desafioautoforce
+
+Solução do desafio *Suporte Front-end* para vaga.
+[Link do Desafio](https://github.com/autoforce/front-end-challenge-suporte)
