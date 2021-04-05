664ebb6e3117ec05a958d8474320b5cb8eef5e22
20171014040038 <gesio.m@academico.ifrn.edu.br>
2021-04-05 02:16:58 -0300

    solução da parte 2

--- Parte2/parte2.js
+++ Parte2/parte2.js
@@ -1,20 +1,13 @@
-// modal.classList.remove('is-open')
-// modal.setAttribute('aria-hidden', false)
-let modalGlobal = document.getElementsByClassName('header-conversion-form-whatsapp-modal')[0]
+let modalPai = document.getElementsByClassName('header-conversion-form-whatsapp-modal')[0]
+modalPai.parentNode.removeChild(modalPai)
 
-let modal = document.getElementsByClassName('modal micromodal-slide')[0]
 let numbers = document.getElementsByClassName('header-mobile__whatsapp-link')
-
-
 for (let i = 0; i < numbers.length; i++) {
-    numbers[i].setAttribute('href', numbers[i].getAttribute('data-link'))
     numbers[i].addEventListener('click', (e) => {
         e.preventDefault()
-        console.log('aaaaaaaaa')
-        if (modal.classList == 'modal micromodal-slide is-open') {
-            modal.classList = 'modal micromodal-slide'
-        }
-
+        console.log(numbers[i].getAttribute('data-link'))
+        numbers[i].setAttribute('href', numbers[i].getAttribute('data-link'))
+        window.location.href = numbers[i].getAttribute('data-link')
     })
 }
 
